110cfa5c4f2115c1a7c2be74cb9fabe19bcc4309
tsubasa <tonochi.tsu@gmail.com>
2019-12-12 17:39:29 +0900

    重複したものを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'toppages#index'
+  get 'contact', to: 'toppages#contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
+  get 'signup', to: 'users#new' 
 
-
-
-  root 'tweets#index'  #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'
-  post  'tweets'      =>  'tweets#create'   
 end
 
