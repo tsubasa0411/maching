a9e8b5b8a955d24000c3a1a0af130aec973c99b4
tsubasa <tonochi.tsu@gmail.com>
2019-12-13 13:47:23 +0900

    ログイン周り実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,10 +9,17 @@ class Users::SessionsController < Devise::SessionsController
     if user && user.authenticate(params[:session][:password])
        # session[:user_id] = user.idをカスタムヘルパーで定義してもの
       log_in user
+       #　三項演算子で条件分岐処理
+       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
   end
-
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,46 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-        attr_accessor :remember_token
+        before_save { self.email = email.downcase }
+        # バリデーションに成功し、実際にオブジェクトが保存される直前で実行されます
+  attr_accessor :remember_token
+  # ログイン時に発行するトークンを保持し、cookieにも同様の値を保持させ、ログインしているかどうかを判別するのに使用します。
+
   validates :nickname, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :profile, length: { maximum: 200}
+  validates :password, presence: true, length: { minimum: 6 }
+
   has_secure_password
+
+# ログイン周りに必要な定義↓
+
+  # 渡された文字列のハッシュ値を返す。Userモデルのクラスメソッドとして作成する
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す。Userモデルのクラスメソッドとして作成する
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+  #先に属性を定義している
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, 
+    User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  # has_secure_passwordで提供されていたauthenticateメソッドと同じようなものを、独自に定義している。
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
