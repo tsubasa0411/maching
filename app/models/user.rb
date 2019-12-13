fd29c8794056b8696de9bd1ef9d740fed9ba6424
tsubasa <tonochi.tsu@gmail.com>
2019-12-13 12:27:22 +0900

    対策

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attr_accessor :remember_token
-  # attr_accessorは、データベースに保存されないカラム(モデルに持たせるデータ)を作る
-  validates :nickname, presence: true, length: { maximum: 50 }
+         
+        attr_accessor :remember_token
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :profile, length: { maximum: 200}
   has_secure_password
-  # パスワードの検証を行えるようにしている
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_112804) do
+ActiveRecord::Schema.define(version: 2019_12_13_023851) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -24,11 +24,13 @@ ActiveRecord::Schema.define(version: 2019_12_12_112804) do
     t.string "password_digest"
     t.string "profile"
     t.string "remember_digest"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
